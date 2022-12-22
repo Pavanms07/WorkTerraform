bedc304b1c0f098d0f83579099deeccf58e5d84f
PAVANms3 <59008808+PAVANms3@users.noreply.github.com>
2022-12-22 20:59:09 +0530

    ss added

--- w.py
+++ w.py
@@ -1,5 +1,5 @@
 from tabulate import tabulate
 
 table = [["Sun",696000,1989100000],["Earth",6371,5973.6],
-        ["Moon",1737,73],["Mars",3390,641.85]]
+        ["Moon",1737,73],["Marss",3390,641.85]]
 print(tabulate(table))
